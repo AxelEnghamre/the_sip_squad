0611128913823cfb65ef837eeae739e08e784b2d
Axel Enghamre <axel@enghamre.se>
2023-04-11 12:48:25 +0200

    styling for displaying not visible

--- src/components/IngredientsList/ListItem/index.tsx
+++ src/components/IngredientsList/ListItem/index.tsx
@@ -1,15 +1,21 @@
-const ListItem = ({ ingredient, toggleIngredientVisibility }: { ingredient: Ingredient; toggleIngredientVisibility: Function }) => {
+const ListItem = ({
+  ingredient,
+  toggleIngredientVisibility,
+}: {
+  ingredient: Ingredient;
+  toggleIngredientVisibility: Function;
+}) => {
   const handleVisibility = () => {
     toggleIngredientVisibility(ingredient.id);
-  }
-  
-return (
+  };
+
+  return (
     <li className="flex h-14 w-full flex-row justify-between gap-2 rounded-xl bg-bright-pink p-2">
       <input
         type="checkbox"
         checked={ingredient.isVisible}
         onClick={handleVisibility}
-        onChange={()=>{}}
+        onChange={() => {}}
         title={
           ingredient.isVisible
             ? `Hide results from ${ingredient.name}`
@@ -17,7 +23,13 @@ return (
         }
         className="h-full w-8 appearance-none rounded-l-lg border-4 border-pale-dogwood bg-bright-pink transition-colors duration-200 checked:bg-pale-dogwood hover:cursor-pointer hover:bg-pale-dogwood hover:checked:bg-bright-pink"
       />
-      <span className="grow bg-pale-dogwood p-2 text-bright-pink">
+      <span
+        className={`grow p-2 transition-colors  duration-200  ${
+          ingredient.isVisible
+            ? "bg-pale-dogwood text-bright-pink"
+            : "bg-bright-pink text-pale-dogwood line-through"
+        }`}
+      >
         {ingredient.name}
       </span>
       <button
