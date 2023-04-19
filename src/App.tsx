f65e9f66a8067ec620cddd40fc765f97e8d1537c
Axel Enghamre <axel@enghamre.se>
2023-04-19 11:06:38 +0200

    updated drinks list

--- src/App.tsx
+++ src/App.tsx
@@ -3,104 +3,80 @@ import IngredientsList from "./components/IngredientsList";
 import InputIngredient from "./components/InputIngredient";
 
 const fetchDrink = async (name: string) => {
-    const API = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
-    const response = await fetch(API + name);
-    const data = await response.json();
+  const API = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
+  const response = await fetch(API + name);
+  const data = await response.json();
 
-    return data;
+  const drinks = data.drinks;
+  const drinksWithIngredients = await Promise.all(
+    drinks.map(async (drink: any) => {
+      const ingredientsResponse = await fetchIngredients(drink.idDrink);
+      const ingredients = ingredientsResponse.drinks[0];
+      return { ...drink, ingredients };
+    })
+  );
+
+  return drinksWithIngredients;
 };
 
 async function fetchIngredients(drinkId: string) {
-    const response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
-    );
-    const data = await response.json();
-    return data;
+  const response = await fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
+  );
+  const data = await response.json();
+  return data;
 }
 
-const fetchDrinksByIngredients = async (ingredients: Ingredient[]) => {
-    const drinkLists = await Promise.all(
-        ingredients.map(async (ingredient) => {
-            const drinksResponse = await fetchDrink(ingredient.name);
-            const drinks = drinksResponse.drinks;
-            const drinksWithIngredients = await Promise.all(
-                drinks.map(async (drink: any) => {
-                    const ingredientsResponse = await fetchIngredients(drink.idDrink);
-                    const ingredients = ingredientsResponse.drinks[0];
-                    return { ...drink, ingredients };
-                })
-            );
-            return { ingredient, drinks: drinksWithIngredients };
-        })
-    );
-
-    const drinkCounts: { [key: string]: number } = {};
-    drinkLists.forEach((list) => {
-        list.drinks.forEach((drink: any) => {
-            if (!drinkCounts[drink.strDrink]) {
-                drinkCounts[drink.strDrink] = 0;
-            }
-            drinkCounts[drink.strDrink]++;
-        });
-    });
-
-    const matchedDrinkNames = Object.keys(drinkCounts).sort((a, b) => {
-        return drinkCounts[b] - drinkCounts[a];
-    });
-
-    //log the drinks and ingredients
-    drinkLists.forEach((list) => {
-        console.log(list.ingredient.name);
-        list.drinks.forEach((drink: any) => {
-            console.log(drink.strDrink);
-            console.log(drink.ingredients);
-        });
-    });
-
-    drinkLists.forEach((list) => {
-        list.drinks.sort((a: any, b: any) => {
-            return matchedDrinkNames.indexOf(a.strDrink) - matchedDrinkNames.indexOf(b.strDrink);
-        });
-    });
-
-    return { matchedDrinkNames, drinkLists };
-};
-
-
 const App = () => {
-    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
-    const [drinks, setDrinks] = useState<string[]>([]);
-    const [drinkLists, setDrinkLists] = useState<any[]>([]);
+  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+  const [drinkLists, setDrinkLists] = useState<any[]>([]);
 
+  useEffect(() => {
+    const visibleIngredients = ingredients.filter((ingredient) => {
+      if (ingredient.isVisible) {
+        return ingredient;
+      }
+    });
 
-    useEffect(() => {
-        const fetchDrinkLists = async () => {
-            const { matchedDrinkNames, drinkLists } = await fetchDrinksByIngredients(filteredIngredients);
-            setDrinks(matchedDrinkNames);
-            setDrinkLists(drinkLists);
-        };
+    let allDrinks: any = [];
+    visibleIngredients.forEach((ingredient) => {
+      allDrinks.push(...ingredient.drinks);
+    });
 
-        const filteredIngredients = ingredients.filter((ingredient) => ingredient.isVisible);
-        if (filteredIngredients.length === 0) {
-            setDrinks([]);
-            setDrinkLists([]);
-            return;
+    let drinks: any = [];
+
+    allDrinks.forEach((drink: any) => {
+      let unique = true;
+      drinks.forEach((sortedDrink: any) => {
+        if (drink.idDrink === sortedDrink.idDrink) {
+          unique = false;
         }
+      });
 
-        fetchDrinkLists();
-    }, [ingredients]);
+      if (unique) {
+        drinks.push(drink);
+      }
+    });
 
-    const addIngredient = (ingredient: string) => {
-        ingredient = ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
-    setIngredients((prevState) => [
-      ...prevState,
-      { name: ingredient, isVisible: true, id: crypto.randomUUID() },
-    ]);
+    setDrinkLists(allDrinks);
+  }, [ingredients]);
+
+  const addIngredient = async (ingredient: string) => {
+    ingredient = ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
+    try {
+      const drinks = await fetchDrink(ingredient);
+      setIngredients((prevState) => [
+        ...prevState,
+        { name: ingredient, isVisible: true, id: crypto.randomUUID(), drinks },
+      ]);
+    } catch (error) {
+      alert(`${ingredient} is not an ingredient!`);
+    }
   };
 
   const toggleIngredientVisibility = (id: string) => {
     const foundIndexKey = ingredients.findIndex(
-        (ingredient) => ingredient.id === id
+      (ingredient) => ingredient.id === id
     );
 
     const updatedIngredients = ingredients.map((ingredient, index) => {
@@ -109,6 +85,7 @@ const App = () => {
           name: ingredient.name,
           id: ingredient.id,
           isVisible: !ingredient.isVisible,
+          drinks: ingredient.drinks,
         };
       }
 
@@ -120,7 +97,7 @@ const App = () => {
 
   const removeIngredient = (id: string) => {
     const foundIndexKey = ingredients.findIndex(
-        (ingredient) => ingredient.id === id
+      (ingredient) => ingredient.id === id
     );
 
     const temp = [...ingredients];
@@ -135,46 +112,63 @@ const App = () => {
     setIngredients(updatedIngredients);
   };
   return (
-      <>
-        <IngredientsList
-            ingredients={ingredients}
-            toggleIngredientVisibility={toggleIngredientVisibility}
-            removeIngredient={removeIngredient}
-        />
-        <InputIngredient onSubmit={addIngredient} />
-        <div>
-            {
-                drinks.map((drink) => {
-                    return (
-                        <div>
-                            <h2 className={"text-3xl"}>{drink}</h2>
-                            <ul className={" list-disc list-inside"}>
-                                {
-                                    drinkLists.map((drinkList) => {
-                                        const drinkObject = drinkList.drinks.find((drinkObject: any) => drinkObject.strDrink === drink);
-                                        if (drinkObject) {
-                                            return (
-                                                Object.keys(drinkObject.ingredients).map((key) => {
-                                                    if (key.startsWith('strIngredient') && drinkObject.ingredients[key]) {
-                                                        return (
-                                                            <li className={ingredients.find((ingredient) => ingredient.name.toLowerCase() === drinkObject.ingredients[key].toLowerCase()) ? 'text-green-500' : 'text-red-500'}>
-                                                                {drinkObject.ingredients[key]} - {drinkObject.ingredients[key.replace('strIngredient', 'strMeasure')]}
-                                                            </li>
-                                                        );
-                                                    }
-                                                })
-                                            );
-                                        }
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    );
-                })
-            }
+    <>
+      <IngredientsList
+        ingredients={ingredients}
+        toggleIngredientVisibility={toggleIngredientVisibility}
+        removeIngredient={removeIngredient}
+      />
+      <InputIngredient onSubmit={addIngredient} />
 
-        </div>
-      </>
+      <ul className="flex flex-row flex-wrap items-center justify-center gap-6">
+        {drinkLists.map((drink) => {
+          return (
+            <li
+              className=" h-fit w-fit rounded-xl bg-celestial-blue p-4"
+              key={crypto.randomUUID()}
+            >
+              <h3 className="text-xl text-bright-pink">{drink.strDrink}</h3>
+              <ul className="flex flex-col gap-2">
+                {Object.keys(drink.ingredients).map((key) => {
+                  if (
+                    key.startsWith("strIngredient") &&
+                    drink.ingredients[key]
+                  ) {
+                    return (
+                      <li
+                        key={crypto.randomUUID()}
+                        className={
+                          ingredients.find((ingredient) => {
+                            if (
+                              ingredient.name.toLowerCase() ===
+                              drink.ingredients[key].toLowerCase()
+                            ) {
+                              if (ingredient.isVisible) {
+                                return 1;
+                              }
+                              return 2;
+                            }
+                          })
+                            ? "text-green-500"
+                            : "text-black"
+                        }
+                      >
+                        {drink.ingredients[key]} -{" "}
+                        {
+                          drink.ingredients[
+                            key.replace("strIngredient", "strMeasure")
+                          ]
+                        }
+                      </li>
+                    );
+                  }
+                })}
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
