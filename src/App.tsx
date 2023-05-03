1173cb73bf8ac16ba0fa947de9a97b70ff2185a3
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-05-03 09:31:27 +0200

    padding for input and list on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,7 @@ const App = () => {
       </div>
 
       <div
-        className="flex min-w-full snap-center flex-col gap-8 bg-zinc-50 pt-24 md:w-1/3 md:min-w-0 md:px-8 md:shadow-md"
+        className="flex min-w-full snap-center flex-col gap-8 bg-zinc-50 px-4 pt-24 md:w-1/3 md:min-w-0 md:px-8 md:shadow-md"
         ref={ingredientsRef}
       >
         <InputIngredient onSubmit={addIngredient} />
